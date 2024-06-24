8331b39fac1f68f08d8b6718cb661e22b0d3e817
eashaan-surve <eashaan.surve@gmail.com>
2024-06-24 12:23:03 -0400

    Initial Commit

---
+++ RoamDemoApp/RoamDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoamDemoAppApp.swift
+//  RoamDemoApp
+//
+//  Created by Eashaan Surve on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct RoamDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
